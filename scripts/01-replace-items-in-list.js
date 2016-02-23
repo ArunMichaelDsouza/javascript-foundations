36b2095361a5937eec144cd6363bc95b4922d60e
Arun Michael Dsouza <amdsouza92@gmail.com>
2016-02-24 01:42:42 +0530

    Algo to replace items in list - pseudo code added

---
+++ scripts/01-replace-items-in-list.js
@@ -0,0 +1,23 @@
+/* Algorithm to replace items in list
+
+Pseudo code : 
+1. begin
+
+2. set initial values in list 
+   list = [457,122,7787]
+
+3. set item to replace from
+   itemToReplace = 122
+
+4. set item to replace to
+   itemToInsert = 199
+
+5. perform linear search in list
+   find(itemToReplace in list)
+
+6. add value to be inserted in list
+   insert(itemToInsert in list[position of itemToReplace])
+
+7. end
+*/
+
