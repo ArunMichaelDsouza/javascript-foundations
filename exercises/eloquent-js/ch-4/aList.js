fe46af4d0626023d5833912a0c930a90dc696a58
Arun Michael Dsouza <amdsouza92@gmail.com>
2017-03-04 15:58:39 +0530

    Helper functions added to list solution

--- exercises/eloquent-js/ch-4/aList.js
+++ exercises/eloquent-js/ch-4/aList.js
@@ -3,11 +3,11 @@
 (function() {
 	function arrayToList(arr) {
 		var list = null;
-		for(var i = arr.length - 1; i >= 0; i --) {
+		for(var i = arr.length -1; i >= 0; i --) {
 			list = {
 				value: arr[i],
 				rest: list
-			};
+			}
 		}
 
 		return list;
@@ -20,6 +20,20 @@
 		}
 
 		return arr;
+	};
+
+	function prepend(value, list) {
+		var obj = {
+			value: value,
+			rest: list
+		};
+
+		return obj;
+	};
+
+	function nth(list, index) {
+		var list = listToArray(list);
+		return list[index] ? list[index] : undefined;
 	};
 
 	var array = [12, 15, 22],
@@ -36,4 +50,6 @@
 
 	console.log(arrayToList(array));
 	console.log(listToArray(list));
+	console.log(prepend(8, list));
+	console.log(nth(list, 2));
 })();
